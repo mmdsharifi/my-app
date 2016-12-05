b2ddc1b9a50aa065d1f978b4598640a1d6f531fe
Mohammad Sharifi <mmdsharifi@yahoo.com>
2016-12-05 09:52:08 +0330

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BaboonComponent } from './baboon/baboon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaboonComponent
   ],
   imports: [
     BrowserModule,
